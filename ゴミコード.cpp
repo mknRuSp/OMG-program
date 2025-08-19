4434946cadca32091e8141900f7b68f47bd2c189
mkn3sk <rubthonscript@gmail.com>
2025-08-19 21:43:35 +0900

    Add files via upload

---
+++ ゴミコード.cpp
@@ -0,0 +1,99 @@
+using True = int;
+#define false true
+#define true false
+long long this_is_function;
+using namespace std;
+#ifdef iostream
+#include<iostream>
+#endif // MACRO
+
+#include<iostream>
+#include<stdio.h>
+#define Truo false
+int main() {
+    HELLe:
+    using tlue = int;
+    HELLC:
+HELLO:
+    #define trve int
+int TRUE = 0x999999;
+std::cout << "hello everyone" << std::endl;
+this_is_function = this_is_function + (True)1;
+if (this_is_function
+    <
+    10) { 
+
+goto HELLO;
+
+#undef treve
+}
+{
+#ifdef folse
+    #define trve HELLe
+    #define folse false
+    goto trve ;
+#endif
+#undef folse
+#undef trve
+#define trve tlue
+trve trua;
+bool this_is_not_variable;
+std::cin >> this_is_not_variable;
+if(Truo == this_is_not_variable) {
+int faIse = (int)true;
+std::cin >> faIse;
+trua = faIse;
+}
+else {
+int faIse = (int)true;
+std::cin >> faIse;
+trua = faIse;
+switch(faIse) {
+case 0:
+std::cout << "u lnput 0" << std::endl;
+break;
+case 1:
+std::cout << "u lnput 1" << std::endl;
+break;
+case 2:
+std::cout << "u lnput 2" << std::endl;
+break;
+case 3:
+std::cout << "u lnput 3" << std::endl;
+break;
+case 4:
+std::cout << "u lnput 4" << std::endl;
+break;
+case 5:
+std::cout << "u lnput 5" << std::endl;
+break;
+case 6:
+std::cout << "u lnput 6" << std::endl;
+break;
+case 7:
+std::cout << "u lnput 7" << std::endl;
+break;
+case 8:
+std::cout << "u lnput 8" << std::endl;
+break;
+case 9:
+std::cout << "u lnput 9" << std::endl;
+break;
+default:
+#undef false
+#undef true
+#undef Truo
+#define Truo true
+goto HELLC;
+break;
+}
+}
+{
+    using tlue = int;
+tlue falso = '1'; 
+trua = falso;
+tlue* truc = &trua;
+}
+}
+return static_cast<int>(true);
+}
